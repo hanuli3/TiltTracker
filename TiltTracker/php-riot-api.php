e06b66e93b9d3e5b5a08def8fa5e412e42f7ea4e
djx7et <djx7et@virginia.edu>
2019-04-25 20:36:57 -0400

    api

--- TiltTracker/php-riot-api.php
+++ TiltTracker/php-riot-api.php
@@ -30,7 +30,7 @@ class riotapi
 	const API_URL_MATCH_4 = 'https://{platform}.api.riotgames.com/lol/match/v4/';
 	const API_URL_LEAGUE_4 = 'https://{platform}.api.riotgames.com/lol/league/v4/';
 	const API_URL_SUMMONER_4 = 'https://{platform}.api.riotgames.com/lol/summoner/v4/';
-	const API_KEY = 'RGAPI-f3bca53c-f77c-41a4-b2a8-b13f3e3a9759';
+	const API_KEY = 'RGAPI-b2533a10-35a7-477d-9ea1-b245d445cc98';
 	// Rate limit for 10 minutes
 	const LONG_LIMIT_INTERVAL = 600;
 	const RATE_LIMIT_LONG = 500;
