0bb6dc56920261a814b5e9bac202457196ab2995
djx7et <djx7et@virginia.edu>
2019-04-30 02:30:10 -0400

    backend

--- Suggestion/Suggestion-Form/src/app/app.component.ts
+++ Suggestion/Suggestion-Form/src/app/app.component.ts
@@ -18,18 +18,16 @@ export class AppComponent {
   constructor(private http: HttpClient) { }
    
   senddata(data) {
-     console.log(data);
+   console.log(data);
 
-     let params = JSON.stringify(data);
+   let params = JSON.stringify(data);
 
-     //this.http.get('http://localhost/cs4640s19/ngphp-get.php?str='+encodeURIComponent(params))
-     this.http.get('http://localhost/TiltTracker/ngphp-get.php?str='+params)
-     //this.http.post('http://localhost/cs4640s19/ngphp-post.php', data)
-     .subscribe((data) => {
-        console.log('Got data from backend', data);
-        this.responsedata = data.toString();
-     }, (error) => {
-        console.log('Error', error);
-     })
-  }
+   this.http.get('http://localhost/TiltTracker/ngphp-get.php?str='+params)
+   .subscribe((data) => {
+      console.log('Got data from backend', data);
+      this.responsedata = data;
+   }, (error) => {
+      console.log('Error', error);
+   })
+}
 }
--- TiltTracker/ngphp-get.php
+++ TiltTracker/ngphp-get.php
@@ -1,9 +1,9 @@
 <?php
-require_once "config.php";
+
 // try commenting out the header setting to experiment how the back end refuses the request
 header('Access-Control-Allow-Origin: http://localhost:4200');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding');
-
+require_once "config.php";
 // retreive data from the request
 $getdata = $_GET['str'];
 
@@ -12,7 +12,7 @@ $link = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 // process data
 // (this example simply extracts the data and restructures them back)
 $request = json_decode($getdata);
-$data_array = json_decode($getdata, true)
+$data_array = json_decode($getdata, true);
 
 $data = [];
 foreach ($request as $k => $v)
@@ -20,7 +20,14 @@ foreach ($request as $k => $v)
    $data[0]['get'.$k] = $v;
 }
 
-$sql = "INSERT INTO summoners (name,phone,email,suggestion,type,reply) VALUES ($data_array["name"],$data_array["phone"],$data_array["email"],$data_array["suggestion"],$data_array["drink],$data_array["sendText"])";
+$name = $data_array['name'];
+$phone = $data_array['phone'];
+$email = $data_array['email'];
+$suggestion = $data_array['suggestion'];
+$drink = $data_array['drink_option'];
+$reply = $data_array['sendtxt'];
+
+$sql = "INSERT INTO suggestions (name,phone,email,suggestion,type,reply) VALUES ('$name',$phone ,'$email','$suggestion','$drink',$reply)";
 $link->query($sql);
 
 // send response (in json format) back the front end
